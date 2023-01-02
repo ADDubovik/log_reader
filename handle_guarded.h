79a5e01eb1464df083ff818519e43737bc5e96c5
AzureAD\AleksandrDubovik <aleksandr.dubovik@finstek.com>
2023-01-02 19:10:10 +0300

    handle_guarded: move semantics fix

--- handle_guarded.h
+++ handle_guarded.h
@@ -9,6 +9,24 @@ public:
 
     ~HandleGuarded();
 
+    HandleGuarded(HandleGuarded&& oth)
+    {
+        auto temp = _handle;
+        _handle = oth._handle;
+        oth._handle = temp;
+    }
+    HandleGuarded(const HandleGuarded&) = delete;
+
+    HandleGuarded& operator=(HandleGuarded&& oth)
+    {
+        auto temp = _handle;
+        _handle = oth._handle;
+        oth._handle = temp;
+
+        return *this;
+    }
+    HandleGuarded& operator=(const HandleGuarded&) = delete;
+
     void Close();
 
     operator bool() const;
